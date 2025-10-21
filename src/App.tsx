f750e993157252ffb89227e405e1c98bdfd41ad1
Pedro Pinheiro <pedroph12@gmail.com>
2025-10-20 22:32:05 -0300

    fix no balanco

--- src/App.tsx
+++ src/App.tsx
@@ -2,56 +2,62 @@ import { useEffect, useState } from "react";
 import * as C from "./App.style";
 import { Item } from "./types/Item";
 import { items } from "./data/items";
-import { categories } from './data/categories'
-import { filterListByMonth, formatCurrentMonth, getCurrentMonth } from "./helpers/dateFilter";
+import { categories } from "./data/categories";
+import {
+  filterListByMonth,
+  formatCurrentMonth,
+  getCurrentMonth,
+} from "./helpers/dateFilter";
 import { TableArea } from "./components/TableArea";
 import { InfoArea } from "./components/InfoArea";
-
-
+import { deflate } from "zlib";
 
 const App = () => {
   const [list, setList] = useState(items);
   const [filtredList, setFiltredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
-  const [income, setIncome] = useState(0)
-  const [expense, setExpense] = useState(0)
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(() => {
     setFiltredList(filterListByMonth(list, currentMonth));
   }, [currentMonth, list]);
-  useEffect(()=>{formatCurrentMonth(currentMonth)},[])
-  const handleMonthChange = (newMonth: string) =>{
-      setCurrentMonth(newMonth)
-      console.log(newMonth, currentMonth)
-  }
+  useEffect(() => {
+    formatCurrentMonth(currentMonth);
+  }, []);
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+    console.log(newMonth, currentMonth);
+  };
   useEffect(() => {
     let incomeCount = 0;
     let expenseCount = 0;
-    for(let i in filtredList){
-      if(categories[filtredList[i].category].expense === true){
-        expenseCount += filtredList[i].value
-        
-      }else{
-        incomeCount += filtredList[i].value
+
+
+    for (let i in filtredList) {
+      if (categories[filtredList[i].category].expense === true) {
+        expenseCount += filtredList[i].value;
+      } else {
+        incomeCount += filtredList[i].value;
       }
-      setIncome(incomeCount)
-      setExpense(expenseCount)
+
     }
-  },[filtredList])
+      setIncome(incomeCount);
+      setExpense(expenseCount);
+  }, [filtredList]);
   return (
     <C.Container>
       <C.Header>
         <C.HaderText>Sistema Financeiro</C.HaderText>
       </C.Header>
       <C.body>
-        <InfoArea 
+        <InfoArea
           currentMonth={currentMonth}
           onMonthChange={handleMonthChange}
           income={income}
           expense={expense}
-        
-        />        
-        
+        />
+
         <TableArea list={filtredList} />
       </C.body>
     </C.Container>
